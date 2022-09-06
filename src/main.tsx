8b7d0856e7a773857eaea73ee7d1a04cfc45f126
Laurent LE LARDEUX <laurent.le-lardeux@inetum.com>
2022-09-06 15:28:35 +0200

    refactoring typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './app/store'
+
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 )
